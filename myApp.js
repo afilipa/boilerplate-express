ead6c009777b2198e0e3afd39d3d35979f4ba2f9
Ana Teixeira <afilipa_teixeira@hotmail.com>
2020-01-13 22:23:05 +0000

    end of section

--- myApp.js
+++ myApp.js
@@ -1,69 +1,61 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
-
+var bodyParser = require("body-parser");
 // --> 7)  Mount the Logger middleware here
-
-
-// --> 11)  Mount the body-parser middleware  here
-
-
-/** 1) Meet the node console. */
-console.log("Hello World");
-
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 app.use((req, res, next) => {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
 });
+// --> 11)  Mount the body-parser middleware  here
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+app.use(bodyParser.urlencoded({ extended: false }));
+/** 1) Meet the node console. */
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-app.get('/', function (req, res) {
-    res.send('Hello Express');
+app.get("/", function(req, res) {
+  res.send("Hello Express");
 });
 
 /** 3) Serve an HTML file */
-app.get("/", function (req, res) {
-    res.sendFile("views/index.html");
+app.get("/", function(req, res) {
+  res.sendFile("views/index.html");
 });
-
 /** 4) Serve static assets  */
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 /** 5) serve JSON on a specific route */
 app.get("/json", (req, res) => {
-    if (process.env.MESSAGE_STYLE == "uppercase")
-        res.json({ message: "HELLO JSON" });
-    else res.json({ message: "Hello json" });
+  if (process.env.MESSAGE_STYLE == "uppercase")
+    res.json({ message: "HELLO JSON" });
+  else res.json({ message: "Hello json" });
 });
 /** 6) Use the .env file to configure the app */
 process.env.MESSAGE_STYLE = "uppercase";
 
 /** 8) Chaining middleware. A Time server */
-app.get("/now",
-    (req, res, next) => {
-        req.time = new Date().toString();
-        next();
-    },
-    (req, res) => res.json({ time: req.time })
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => res.json({ time: req.time })
 );
-
-
 /** 9)  Get input from client - Route parameters */
 app.get("/:word/echo", (req, res) => res.json({ echo: req.params.word }));
-
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
-
-/** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
-
+app.get("/name", (req, res) =>
+  res.json({ name: req.query.first + " " + req.query.last })
+);
 
 /** 12) Get data form POST  */
-
-
+app.post("/name", (req, res) =>
+  res.json({ name: req.body.first + " " + req.body.last })
+);
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
