f7c00a249aa324296a6122f8cc86838b21121d27
Ana Teixeira <afilipa_teixeira@hotmail.com>
2020-01-13 21:23:49 +0000

    ex5

--- myApp.js
+++ myApp.js
@@ -25,7 +25,7 @@ app.get("/", function (req, res) {
 app.use(express.static('public'));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', (req,res)=> res.json({"message": "Hello json"}));
 
 /** 6) Use the .env file to configure the app */
 
